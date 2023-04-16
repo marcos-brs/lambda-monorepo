75a4f6884d125d14f9aff7a99455424ff2d0abef
Marcos Santana <brmarcossantanabr@gmail.com>
2023-04-15 21:14:01 -0300

    fix: remove dist folder from git

--- packages/@common-infra/file-uploader/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class FileUploader {
-    main(data: any): void;
-}
--- packages/@lambdas/lambda-1/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { Context, APIGatewayProxyResult } from 'aws-lambda';
-export declare const handler: (event: import("aws-lambda").APIGatewayProxyEvent, context: Context) => Promise<APIGatewayProxyResult>;
