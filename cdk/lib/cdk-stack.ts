6a1361b9c559ff7a1cb09cfd7a335123e9578e0f
Marcos Santana <brmarcossantanabr@gmail.com>
2023-04-15 21:22:01 -0300

    refactor: remove unnecessary comments

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -10,15 +10,6 @@ import { lambdas } from '../../packages/@lambdas';
 export class LambdaMonorepoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-/*
-    new Function(this, 'function-1', {
-        handler: 'dist/index.handler',
-        runtime: Runtime.NODEJS_18_X,
-        code: Code.fromAsset(path.resolve(__dirname, '..', 'packages', '@lambdas', 'lambda-1'))
-      });
-      */
-
-    // lambda function
 
     for(const {name} of lambdas) {
         const lambda = new NodejsFunction(this, name, {
@@ -32,6 +23,15 @@ export class LambdaMonorepoStack extends cdk.Stack {
             }
         });
 
+        /* 
+        Need to setup esbuild
+        const lambda = new Function(this, name, {
+            handler: 'dist/index.handler',
+            runtime: Runtime.NODEJS_18_X,
+            code: Code.fromAsset(path.join(__dirname, '..', '..', 'packages', '@lambdas', name))
+        });
+        */
+
         const lambdaUrl = lambda.addFunctionUrl({
             authType: FunctionUrlAuthType.NONE,
             cors: {
@@ -43,28 +43,5 @@ export class LambdaMonorepoStack extends cdk.Stack {
             value: lambdaUrl.url,
            });
     }
-
-    // const lambda1 = new NodejsFunction(this, "lambda-1", {
-    //     runtime: Runtime.NODEJS_18_X,
-    //     entry: path.join(__dirname, '..', '..', 'packages', '@lambdas', 'lambda-1', 'src', 'index.ts'),
-    //     handler: "handler",
-    //     bundling: {
-    //         sourceMap: true,
-    //         minify: false,
-    //         sourceMapMode: SourceMapMode.INLINE,
-    //     }
-    // });
-
-  
-    // const myFunctionUrl = lambda1.addFunctionUrl({
-    //     authType: FunctionUrlAuthType.NONE,
-    //     cors: {
-    //         allowedOrigins: ['*'],
-    //     }
-    // });
-  
-    // new cdk.CfnOutput(this, 'FunctionUrl', {
-    //  value: myFunctionUrl.url,
-    // });
   }
 }
